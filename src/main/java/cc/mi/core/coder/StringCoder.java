c2dbae64fd5da0011c7eef3f7ae976c1c228448b
gy <misschuer@163.com>
2017-07-06 15:18:54 +0800

    增加bytebuf读写string的方法

---
+++ src/main/java/cc/mi/core/coder/StringCoder.java
@@ -0,0 +1,24 @@
+package cc.mi.core.coder;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+
+public class StringCoder {
+	public static final Charset UTF8 = Charset.forName("UTF-8");
+	private StringCoder() {}
+	
+	public static void writeString(ByteBuf buffer, String str) {
+		byte[] bytes = str.getBytes(UTF8);
+		buffer.writeShort(bytes.length);
+		buffer.writeBytes(bytes);
+	}
+	
+	public static String readString(ByteBuf buffer) {
+		int size = buffer.readUnsignedShort();
+		byte[] bytes = new byte[size];
+		buffer.readBytes(bytes);
+		
+		return new String(bytes, UTF8);
+	}
+}
