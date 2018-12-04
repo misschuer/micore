a1acb5d25cdd866f13788ba83c0693016cd81972
Your Name <you@example.com>
2018-12-04 17:16:17 +0800

    抽象方法

--- src/main/java/cc/mi/core/manager/ServerManager.java
+++ src/main/java/cc/mi/core/manager/ServerManager.java
@@ -1,6 +1,13 @@
 package cc.mi.core.manager;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import cc.mi.core.callback.Callback;
 import cc.mi.core.generate.msg.AddTagWatchAndCall;
@@ -8,10 +15,12 @@ import cc.mi.core.generate.msg.AddWatchAndCall;
 import cc.mi.core.generate.msg.IdentityServerMsg;
 import cc.mi.core.generate.msg.ServerRegOpcode;
 import cc.mi.core.generate.msg.StartReady;
+import cc.mi.core.handler.Handler;
 import cc.mi.core.log.CustomLogger;
 import cc.mi.core.packet.Packet;
 import cc.mi.core.server.ContextManager;
 import cc.mi.core.server.ServerConnList;
+import cc.mi.core.utils.ServerProcessBlock;
 import io.netty.channel.Channel;
 
 public abstract class ServerManager {
@@ -25,12 +34,106 @@ public abstract class ServerManager {
 	
 	private final ServerConnList connList = new ServerConnList();
 	
+	// 消息收到以后的回调
+	protected final Map<Integer, Handler> handlers = new HashMap<>();
 	// 需要的消息opcode列表
-	private final List<Integer> opcodes;
+	protected final List<Integer> opcodes = new LinkedList<>();
+	// 帧刷新
+	protected final ScheduledExecutorService excutor = Executors.newScheduledThreadPool(1);
+	// 消息包队列
+	protected final Queue<Packet> packetQueue = new LinkedList<>();
+	// 当前帧刷新执行的代码逻辑
+	protected ServerProcessBlock process;
+	// 最后一次执行帧刷新的时间戳
+	protected long timestamp = 0;
 	
-	public ServerManager(int serverType, List<Integer> opcodes) {
+	
+	public ServerManager(int serverType) {
 		this.serverType = serverType;
-		this.opcodes = opcodes;
+		
+		this.onOpcodeInit();
+		this.onExecutorInit();
+		this.onProcessInit();
+	}
+	
+	/**
+	 * 消息号处理
+	 */
+	protected abstract void onOpcodeInit();
+	
+	/**
+	 * 定时任务处理
+	 */
+	protected void onExecutorInit() {
+		final ServerManager instance = this;
+		excutor.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				long prev = instance.timestamp;
+				long now = System.currentTimeMillis();
+				int diff = 0;
+				if (prev > 0) diff = (int) (now - prev);
+				instance.timestamp = now;
+				if (diff < 0 || diff > 1000) {
+					logger.warnLog("too heavy logical that execute");
+				}
+				try {
+					instance.doWork(diff);
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+		}, 1000, 100, TimeUnit.MILLISECONDS);
+	}
+	
+	/**
+	 * update 过程处理
+	 */
+	protected void onProcessInit() {}
+	
+	/**
+	 * 定时任务处理逻辑
+	 * @param diff
+	 */
+	protected abstract void doWork(int diff);
+	
+	
+	protected void doProcess(int diff) {
+		if (this.process != null) {
+			this.process.run(diff);
+		}
+	}
+	
+	/**
+	 * 处理包信息
+	 */
+	protected void dealPacket() {
+		while (!packetQueue.isEmpty()) {
+			Packet packet = packetQueue.poll();
+			this.invokeHandler(packet);
+		}
+	}
+	
+	/**
+	 * 调用具体消息包处理方法
+	 * @param packet
+	 */
+	protected void invokeHandler(Packet packet) {
+		int opcode = packet.getOpcode();
+		Handler handle = handlers.get(opcode);
+		if (handle != null) {
+			handle.handle(null, this.centerChannel, packet);
+		}
+	}
+	
+	/**
+	 * 添加信息
+	 * @param packet
+	 */
+	public void pushPacket(Packet packet) {
+		synchronized (this) {
+			packetQueue.add(packet);
+		}
 	}
 	
 	/**
